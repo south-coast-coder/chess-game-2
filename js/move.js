a153006ce36bc84056cc06a7ce72c89d7a822eb7
south-coast-coder <charliejarman1989@yahoo.com>
2022-10-17 21:04:52 +0100

    managed to get pawns to take each other although one funciton causes them to vanish so need to work on that

--- js/move.js
+++ js/move.js
@@ -6,30 +6,36 @@ function move(square){
 	var turn = localStorage.getItem("turn")
 	var targetSquare=(square)   // load the last clicked piece (current), the square it was on and the target square
 	if (turn == 'white'){
+		alert("White's turn")
 		var firstLetter = "p"
 		var piece = "rook.png"
 	}
 	else{
+		alert("Black's turn")
 		var firstLetter="b"
 		var piece = "rook2.png"
 	}               //check whose turn it is 
-	if (turn == "white" & current[0]!=="p"){
+	alert("current=" + current)
+	if (turn == "white" & current[0]!=="p"){   //check currently selected piece is of the right colour
+		alert("current "+current)
 		alert("select a piece first!")
 		return
 	}
 	if (turn == "black" & current[0]!=="b"){
+		alert("current "+current)
 		alert("select a piece first!")
 		return
 	}
-	if (document.getElementById(square).firstElementChild == null){
+	if (document.getElementById(square).firstElementChild == null){ // !!!! Broken here - code runs but piece simply vanishes - problem must be in inner HTML call
 		alert("empty")
 		alert("current" + current)
 		var targ=(document.getElementById(targetSquare))
 		alert("target" +targ.id)
-		document.getElementById(targetSquare).innerHTML = '<img src="'+piece+'"alt="" class="piece" id='+ current +' onclick="clicked(current,targ)">'; //puts piece in new square
+		document.getElementById(targetSquare).innerHTML = '<img src="'+piece+'"alt="" class="piece" id='+ current +' onclick="clicked('+current +','+targ+')>'; //puts piece in new square
         document.getElementById(lastSquare).innerHTML = '' //removes piece from old square
          if (turn =="white"){
     	localStorage.setItem("turn","black")
+    	return
     }
     if (turn =="black"){
     	localStorage.setItem("turn","white")
@@ -48,7 +54,7 @@ function move(square){
 	alert(firstLetter)
 	var name=document.getElementById(square).firstElementChild
 	alert("Black or White: " + name.id[0] )
-	if (name.id[0]==firstLetter)
+	if (name.id[0]==firstLetter) //check if trying to take own piece!
 	{
 		alert("Own Piece!")
 		return 
--- js/turn.js
+++ js/turn.js
@@ -2,4 +2,10 @@ function turn(){
 	localStorage.setItem("turn","white")
 	var turn = localStorage.getItem("turn")
 	alert("it is " + turn + "s turn")
+	var lastPiece = localStorage.getItem("currentPiece") //clear current piece from local storage
+	if(lastPiece !=null){
+		localStorage.setItem("currentPiece","")
+	}
+	alert(localStorage.getItem("currentPiece"))
+
 }
